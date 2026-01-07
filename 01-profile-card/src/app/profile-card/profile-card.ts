bcb87e32fc95c8f4b4d39f68a65715e661dde817
Mahmoud-Abbes <mahmoud.abbes.155@gmail.com>
2026-01-07 11:34:50 +0100

    [01-profile-card] Add initial data and import for ProfileCard component

--- 01-profile-card/src/app/profile-card/profile-card.ts
+++ 01-profile-card/src/app/profile-card/profile-card.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss',
 })
-export class ProfileCard {}
+export class ProfileCard {
+  name: string = 'Mahmoud Abbes';
+  age: number = 21;
+  description: string = 'Passionnate web developper looking to boost his skills';
+}
