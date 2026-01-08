aa438d8fe8d11eb4dec86afd905124f38282cbc6
Mahmoud-Abbes <mahmoud.abbes.155@gmail.com>
2026-01-08 18:26:17 +0100

    [04-stopwatch] Add UI and styling to Stopwatch component

--- 04-stopwatch/src/app/app.ts
+++ 04-stopwatch/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Stopwatch } from './stopwatch/stopwatch';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Stopwatch],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
