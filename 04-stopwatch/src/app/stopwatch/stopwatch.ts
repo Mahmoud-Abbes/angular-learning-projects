5a6e1704cbb222009984e882ee45f4cd140ee951
Mahmoud-Abbes <mahmoud.abbes.155@gmail.com>
2026-01-08 18:12:01 +0100

    [04-stopwatch] Add initial data, logic, and import for Stopwatch component

--- 04-stopwatch/src/app/stopwatch/stopwatch.ts
+++ 04-stopwatch/src/app/stopwatch/stopwatch.ts
@@ -1,9 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-stopwatch',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stopwatch.html',
   styleUrl: './stopwatch.scss',
 })
-export class Stopwatch {}
+export class Stopwatch {
+  intervalRef: any;
+  isRunning: boolean = false;
+  elapsedTime: number = 0;
+
+  start() {
+    this.isRunning = true;
+    this.intervalRef = setInterval(() => {
+      this.elapsedTime += 0.1;
+    }, 100);
+  }
+
+  stop() {
+    clearInterval(this.intervalRef);
+    this.isRunning = false;
+  }
+
+  reset() {
+    this.elapsedTime = 0;
+  }
+}
