e4da73e22f6c1b9589206c3a71d258271eb2fb00
Mahmoud-Abbes <mahmoud.abbes.155@gmail.com>
2026-01-08 15:04:24 +0100

    [03-simple-todo] Add initial data, logic, and import for Todo list component

--- 03-simple-todo/src/app/todo-list/todo-list.ts
+++ 03-simple-todo/src/app/todo-list/todo-list.ts
@@ -1,9 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.scss',
 })
-export class TodoList {}
+export class TodoList {
+  taskList: string[] = [];
+  currentTask: string = '';
+
+  addTask() {
+    if (this.currentTask.trim()) {
+      this.taskList.push(this.currentTask);
+      this.currentTask = '';
+    }
+  }
+
+  clearTasks() {
+    this.taskList.length = 0;
+  }
+
+  deleteTask(index: number) {
+    this.taskList = this.taskList.filter((el, i) => i !== index);
+  }
+}
